393272f23f1597e2d2cb7f2557d77f7fe856b158
anvarovagulnaz <anvarovagulnaz@gmail.com>
2021-12-28 15:52:41 +0500

    Добавьте файлы проекта.

---
+++ Lesson15_15/ArithProgression.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson15_15
+{
+    class ArithProgression
+    {
+        interface ISeries
+        { }
+            int step;
+            int startValue;
+            int currentValue;
+
+            public int getNext()
+            {
+                currentValue += step;
+                return currentValue;
+            }
+
+            public void reset()
+            {
+                currentValue = startValue;
+            }
+
+            public void setStart(int x)
+            {
+                startValue = x;
+                currentValue = startValue;
+            }
+            public void setStep(int s)
+            {
+                step = s;
+            }
+           
+            }
+        }
+    
+
+
